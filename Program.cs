8e40a83513535aeb4e2faf217121826eb439b6c9
Jpablo851 <jpablo851@hotmail.com>
2022-08-30 19:26:57 -0500

    nombre actulizado

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ namespace appbanco
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("pepito perez");
         }
     }
 }
